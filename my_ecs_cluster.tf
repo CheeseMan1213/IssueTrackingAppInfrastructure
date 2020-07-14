1263da163a6226fc239cc138359311899136a5ee
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-07-14 16:20:18 -0500

    Ran command 'terraform tmt'. Removed a comment. In resource.aws_ecs_service.frontend_service.task_definition, changed to '.family' instead of '.arn'.

--- my_ecs_cluster.tf
+++ my_ecs_cluster.tf
@@ -21,9 +21,10 @@ EOF
 }
 
 resource "aws_ecs_service" "frontend_service" {
-  name            = "frontend_service"
-  cluster         = module.ecs.this_ecs_cluster_id
-  task_definition = aws_ecs_task_definition.frontend_task_3.arn
+  name    = "frontend_service"
+  cluster = module.ecs.this_ecs_cluster_id
+  # task_definition = aws_ecs_task_definition.frontend_task_3.arn
+  task_definition = aws_ecs_task_definition.frontend_task_3.family
   launch_type     = "FARGATE"
   // I got an error saying that services only need the IAM role if there is a load balancer.
   // iam_role        = "arn:aws:iam::475640621870:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
@@ -34,7 +35,6 @@ resource "aws_ecs_service" "frontend_service" {
   deployment_minimum_healthy_percent = 0
 
   network_configuration {
-    // security_groups = ["${var.awsvpc_service_security_groups}"]
     security_groups  = [module.vpc.default_security_group_id]
     subnets          = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
     assign_public_ip = true
