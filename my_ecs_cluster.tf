21b454e86942853f1f78bb1cb7da314db0692894
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-07-27 09:13:04 -0500

    Added the creation of a CloudWatch log group called 'frontend_task_5'. Changed the names of my 2 task definitions 'frontend_task_3' to 'frontend_task_5' and 'backend_task_1' to 'backend_task_2' . Changed my task definitions to use EC2 instead of Fargate, commented out their cpu and memory requirements, added port mappings. Added a load balancer, but commented it out.

--- my_ecs_cluster.tf
+++ my_ecs_cluster.tf
@@ -1,42 +1,69 @@
-resource "aws_ecs_task_definition" "frontend_task_3" {
-  family                   = "frontend_task_3"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
+resource "aws_cloudwatch_log_group" "frontend_task_5" {
+  name = "frontend_task_5"
+
+  tags = merge(local.common_tags, { Name = "issueTracking-${local.env_name}_log_group" })
+  # tags = merge(local.common_tags, { Name })
+}
+
+resource "aws_ecs_task_definition" "frontend_task_5" {
+  family                   = "frontend_task_5"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc" # valid choices are: none, bridge, awsvpc, or host
   // task_role_arn = "value"
   execution_role_arn = "arn:aws:iam::475640621870:role/ecsTaskExecutionRole"
-  cpu                = 1024 // 1 vCPU
-  memory             = 2048
+  # cpu                = 256 // 0.25 vCPU
+  # memory             = 512
 
   container_definitions = <<EOF
 [
   {
-    "name": "frontend_task_3",
-    "image": "475640621870.dkr.ecr.us-east-1.amazonaws.com/issue-tracking-ecr-repo:latest",
-    "cpu": 1024,
-    "memory": 2048,
-    "essential": true
+    "name": "frontend_task_5",
+    "image": "475640621870.dkr.ecr.us-east-1.amazonaws.com/issue-tracking-ecr-frontend:latest",
+    "cpu": 256,
+    "memory": 512,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${aws_cloudwatch_log_group.frontend_task_5.name}",
+        "awslogs-region": "us-east-1",
+        "awslogs-stream-prefix": "frontend_task_5"
+      }
+    }
   }
 ]
 EOF
 }
 
-resource "aws_ecs_task_definition" "backend_task_1" {
-  family                   = "backend_task_1"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
+resource "aws_ecs_task_definition" "backend_task_2" {
+  family                   = "backend_task_2"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc" # valid choices are: none, bridge, awsvpc, or host
   // task_role_arn = "value"
   execution_role_arn = "arn:aws:iam::475640621870:role/ecsTaskExecutionRole"
-  cpu                = 1024 // 1 vCPU
-  memory             = 2048
+  # cpu                = 256 // 0.25 vCPU
+  # memory             = 512
 
   container_definitions = <<EOF
 [
   {
-    "name": "backend_task_1",
+    "name": "backend_task_2",
     "image": "475640621870.dkr.ecr.us-east-1.amazonaws.com/issue-tracking-ecr-backend:latest",
-    "cpu": 1024,
-    "memory": 2048,
-    "essential": true
+    "cpu": 256,
+    "memory": 512,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080
+      }
+    ]
   }
 ]
 EOF
@@ -46,10 +73,11 @@ resource "aws_ecs_service" "frontend_service" {
   name    = "frontend_service"
   cluster = module.ecs.this_ecs_cluster_id
   # task_definition = aws_ecs_task_definition.frontend_task_3.arn
-  task_definition = aws_ecs_task_definition.frontend_task_3.family
-  launch_type     = "FARGATE"
-  // I got an error saying that services only need the IAM role if there is a load balancer.
-  // iam_role        = "arn:aws:iam::475640621870:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+  task_definition = aws_ecs_task_definition.frontend_task_5.family
+  launch_type     = "EC2"
+  // I got an error saying that services only needs the IAM role if there is a load balancer.
+  // However, do not comment this back in if your task definition uses the awsvpc network mode.
+  // iam_role = "arn:aws:iam::475640621870:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
 
   desired_count = 1
 
@@ -57,9 +85,20 @@ resource "aws_ecs_service" "frontend_service" {
   deployment_minimum_healthy_percent = 0
 
   network_configuration {
-    security_groups  = [module.vpc.default_security_group_id]
-    subnets          = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
-    assign_public_ip = true
+    security_groups = [module.vpc.default_security_group_id]
+    subnets         = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
+    # Must be commented out when launch type is EC2.
+    # assign_public_ip = true
+  }
+
+  # load_balancer {
+  #   target_group_arn = module.alb.target_group_arns[0]
+  #   container_name   = "frontend_task_3"
+  #   container_port   = 80
+  # }
+
+  deployment_controller {
+    type = "ECS" // Valid values: CODE_DEPLOY, ECS, EXTERNAL. Default: ECS
   }
 }
 
@@ -67,8 +106,8 @@ resource "aws_ecs_service" "backend_service" {
   name    = "backend_service"
   cluster = module.ecs.this_ecs_cluster_id
   # task_definition = aws_ecs_task_definition.backend_task_1.arn
-  task_definition = aws_ecs_task_definition.backend_task_1.family
-  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.backend_task_2.family
+  launch_type     = "EC2"
   // I got an error saying that services only need the IAM role if there is a load balancer.
   // iam_role        = "arn:aws:iam::475640621870:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
 
@@ -78,9 +117,14 @@ resource "aws_ecs_service" "backend_service" {
   deployment_minimum_healthy_percent = 0
 
   network_configuration {
-    security_groups  = [module.vpc.default_security_group_id]
-    subnets          = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
-    assign_public_ip = true
+    security_groups = [module.vpc.default_security_group_id]
+    subnets         = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
+    # Must be commented out when launch type is EC2.
+    # assign_public_ip = true
+  }
+
+  deployment_controller {
+    type = "ECS" // Valid values: CODE_DEPLOY, ECS, EXTERNAL. Default: ECS
   }
 }
 
