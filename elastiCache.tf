aa59ef6d5b59e7b2c9f42cbf32e0ea8487846a2d
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-09-27 13:35:20 -0500

    Added lines for 'subnet_group_name' and 'security_group_name'. This was done to place the ElastiCache Redis Cluster inside the 'issue_tracking_app-development-vpc'.

--- elastiCache.tf
+++ elastiCache.tf
@@ -6,6 +6,8 @@ resource "aws_elasticache_cluster" "eb_alb_redis_cache" {
   parameter_group_name = "default.redis5.0"
   engine_version       = "5.0.6"
   port                 = 6379
+  subnet_group_name    = "issue-tracking-subnet-group"
+  security_group_ids   = ["sg-081c1d2cdad13f62c"]
 
   apply_immediately = true
 }
